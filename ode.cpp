43f2f2f840ff709ab9e9c012ccad1b8b31f12e01
damoreno17 <42391091+damoreno17@users.noreply.github.com>
2018-11-17 13:35:07 -0500

    Update ode.cpp

--- ode.cpp
+++ ode.cpp
@@ -3,26 +3,26 @@
 #include<math.h>
 #include<fstream>
 using namespace std;
-//funcion para calcular la aceleracion en x:
+//funcion  aceleracion en x:
 float a_x(float v_x,float v_y){
 	float a_x=-sqrt(pow(v_x,2)+pow(v_y,2))*v_x;
 	return a_x;
 }
-//funcion para calcular la aceleracion en y:
+//funcion  aceleracion en y:
 float a_y(float v_x, float v_y){
 	float a_y=-10-sqrt(pow(v_x,2)+pow(v_y,2))*v_y;
 	return a_y;
 }
 
 int main(){
-	//las consatntes del problema:
+	//Constantes:
 	float v_i=300, x=0, y=0, delta_t=0.0001, tetha=0.7853981634, i=0;
 	
 	float v_i_x=v_i*cos(tetha);
 	float v_i_y=v_i*sin(tetha);
 	float v_x_medio=v_i_x-(delta_t/2)*a_x(v_i_x, v_i_y);
 	float v_y_medio=v_i_y-(delta_t/2)*a_y(v_i_x, v_i_y);
-	// hacemos un bluque while para calcular las posiciones:
+	// hacemos while para calcular las posiciones:
 	ofstream proyectil("proyectil.txt");
 	while (y>=0){
 		v_i_x=v_x_medio+(delta_t)*a_x(v_i_x, v_i_y);//velocidad en x en t-deltat/2:
